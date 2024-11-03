bc6bf9e18b75f3398d551f4fd1e4d977509de013
marwayehiashaaban2 <marwashaaban21@gmail.com>
2024-11-03 12:18:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  return (
+  
+      <>
+        <h1 className="title">
+            you will achieve all goals  i promise my self  
+        </h1>
+        <h2>
+          marwa love his self
+        </h2>
+      </>
+  
+  );
+}
+
+export default App;
